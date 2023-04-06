2e275251820c900f26b625c65ceaa9a8268addef
Gal Aviezri <g.aviezri@gmail.com>
2023-04-06 19:43:40 +0300

    initial commit

---
+++ TODOserver/src/Main.java
@@ -0,0 +1,20 @@
+import java.io.IOException;
+import java.net.ServerSocket;
+
+public class Main {
+    public static class Server {
+        public static void main(String[] args) {
+            try {
+                ServerSocket serverSocket = new ServerSocket(8080);
+                System.out.println("Server started");
+                while (true) {
+
+                    // new Thread(new ClientHandler(serverSocket.accept())).start();
+                }
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+
+    }
+}
